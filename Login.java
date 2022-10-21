55fbc16bfe64b9122b8681ce25e05bbccbedad29
AmitDave22 <112653204+AmitDave22@users.noreply.github.com>
2022-10-21 00:22:00 -0700

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,92 +1,36 @@
-import java.io.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 
-public class Login {
-    private String username, password;
-    int user_type;
+public class ClassProductList extends ArrayList<Product> {
 
-    private UserInfoItem.USER_TYPE UserType = UserInfoItem.USER_TYPE.Buyer;
-
-
-    public boolean getLogin() {
-        boolean login = true;
-        Scanner sc = new Scanner(System.in);
-        while (login) {
-            System.out.println("===== Login =====");
-            System.out.println("Enter your Username:");
-            username = sc.nextLine();
-            System.out.println("Enter your Password:");
-            password = sc.nextLine();
-            System.out.println("Enter Integer (1 or 2):\n1. Buyer\n2. Seller\n(Enter 0 to exit the login system)\nEnter your choice:");
-            user_type = Integer.parseInt(sc.nextLine());
-            switch (user_type) {
-                case 1:
-                    login = false;
-                    UserType = UserInfoItem.USER_TYPE.Buyer;
-                    break;
-                case 2:
-                    UserType = UserInfoItem.USER_TYPE.Seller ;
-                    login = false;
-                    break;
-                case 0:
-                    System.exit(0);
-                default:
-                    System.out.println("Enter a valid choice! (0,1,2)");
-                    break;
-            }
-            if (checkUser(username, password, user_type)) {
-                login = false;
-                return true;
-            }
-            else
-            {
-                System.out.println("Authentication Failed! Incorrect Username/Password");
-                login = true;
-            }
-        }
-        return false;
+    ClassProductList() {
+        InitializeFromFile();
     }
 
-    public boolean checkUser(String uname, String pwd, int role) {
-        boolean valid = false;
-        FileReader fr = null;
+    void InitializeFromFile() {
         try {
-            if (role == 1) {
-
-                fr = new FileReader("BuyerInfo.txt");
-            } else {
-                fr = new FileReader("SellerInfo.txt");
+            String data[];
+            System.out.println("Successfully initialized the file. ");
+            BufferedReader file;
+            String productName, productCategory, line;
+            int i = 0;
+            file = new BufferedReader(new FileReader("ProductInfo.txt"));
+            while ((line = file.readLine()) != null) {
+                data = line.split(":");
+                productCategory = data[0];
+                productName = data[1];
+                Product theProduct = new Product(productCategory, productName);
+                theProduct.setId(i);
+                i++;
+                add(theProduct);
             }
-            BufferedReader bufferedReader = new BufferedReader(fr);
-            String line;
-            String k[] = new String[2];
-            
-            while ((line = bufferedReader.readLine()) != null) {
-                k = line.split(":");
-                if (k[0].compareTo(uname) == 0 && k[1].compareTo(pwd) == 0) {
-                    fr.close();
-                    bufferedReader.close();
-                    return true;
-                }
-            }
-            fr.close();
-            bufferedReader.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (Exception ignored) {
         }
-
-        return valid;
     }
 
-    public UserInfoItem.USER_TYPE getUserType() {
-        return UserType;
+    public void accept(NodeVisitor visitor) {
+        visitor.visitClassProductList(this);
     }
 
-    public int getUser_type() {
-        return user_type;
-    }
-
-    public String getUname() {
-        return username;
-    }
 }
