c6d21afac586c20cafecf08ddeea218dbd1013d7
AmitDave22 <112653204+AmitDave22@users.noreply.github.com>
2022-10-21 00:33:32 -0700

    Update ProductIterator.java

--- ProductIterator.java
+++ ProductIterator.java
@@ -1,21 +1,48 @@
-public class ProductIterator {
+import java.util.Iterator;
 
-	private ClassProductList classProductList;
+public class ProductIterator implements Iterator { //Iterator Design Pattern
 
-	public boolean hasNext() {
-		return false;
-	}
+    private ClassProductList classProductList;
+    private int currentProductNumber = -1;
 
-	public Product Next() {
-		return null;
-	}
+    ProductIterator(ClassProductList productList)
+    {
+        classProductList = productList;
+    }
+    public boolean hasNext() {
+        return currentProductNumber < classProductList.size() - 1;
+    }
 
-	public void MoveToHead() {
+    public Product next() {
+        if(hasNext())
+        {
+            currentProductNumber++;
+            return  classProductList.get(currentProductNumber);
+        }
+        else {
+            return null;
+        }
+    }
+    public Product next(String pname)
+    {
+        Product product = next();
+        while(product != null){
 
-	}
+            if(product.toString().compareToIgnoreCase(pname) == 0)
+            {
+                return product;
+            }
+            product = next();
+        }
+        return null;
+    }
+    public void MoveToHead() {
+        currentProductNumber = -1;
+    }
 
-	public void Remove() {
+    public void remove() {
+        classProductList.remove(currentProductNumber);
 
-	}
+    }
 
 }
